c479ade67c966b5d4295d1021dc660aec008c186
SNatty98 <mrnatt98@gmail.com>
2025-06-24 18:00:17 +0100

    Front end changes mostly complete. Some sections commented out for now.

--- frontend/src/challenge/MCQChallenge.jsx
+++ frontend/src/challenge/MCQChallenge.jsx
@@ -1,5 +1,53 @@
 import "react"
+import {useState} from "react";
 
-export function MCQChallenge() {
-    return <></>
+export function MCQChallenge({challenge, showInfo = false}) {
+
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [shouldShowInfo, setShouldShowInfo] = useState (showInfo)
+
+    const options = typeof challenge.options === "string"
+        ? JSON.parse(challenge.options)
+        : challenge.options
+
+    const handleOptionSelect = (index) => {
+        if(selectedOption !== null) return;
+        setSelectedOption(index)
+        setShouldShowInfo(true)
+    }
+
+    const getOptionClass = (index) => {
+        if (selectedOption === null) return "option"
+
+        if (index === challenge.correct_answer_id){
+            return "option correct"
+        }
+
+        if (selectedOption === index && index !== challenge.correct_answer_id) {
+            return "option incorrect"
+        }
+
+        return "option"
+    }
+
+    return <div className="challenge-display">
+        <p><strong>Difficulty</strong>: {challenge.difficulty}</p>
+        <p className="challenge-title">{challenge.title}</p>
+        <div className="options">
+            {options.map((option, index) =>(
+                <div
+                    className={getOptionClass((index))}
+                    key={index}
+                    onClick={() => handleOptionSelect(index)}>
+                    {option}
+                </div>
+            ))}
+        </div>
+        {shouldShowInfo && selectedOption !== null && (
+            <div className="explanation">
+                <h4>Challenge Explanation</h4>
+                <p>{challenge.explanation}</p>
+            </div>
+        )}
+    </div>
 }
--- frontend/src/history/HistoryInfo.jsx
+++ frontend/src/history/HistoryInfo.jsx
@@ -1,5 +1,43 @@
 import "react"
+import {useState, useEffect} from "react";
+import {MCQChallenge} from "../challenge/MCQChallenge.jsx";
 
 export function HistoryInfo() {
-    return <></>
+    const [history, setHistory] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetchHistory()
+    }, []);
+
+    const fetchHistory = async () => {
+        setLoading(false);
+    }
+
+    if (loading) {
+       return <div className="loading"> Loading history</div>
+    }
+
+    if (error) {
+        return <div className="error">
+            <p>{error}</p>
+            <button onClick={fetchHistory}>Retry</button>
+        </div>
+    }
+
+    return <div className="history-panel">
+        <h2>History</h2>
+        {history.length === 0 ? <p>No Challenge History</p> :
+            <div className="history-list">
+                {history.map((challenge) => {
+                    return <MCQChallenge
+                                challenge={challenge}
+                                key={challenge.id}
+                                showInfo
+                            />
+            })}
+            </div>
+        }
+    </div>
 }
